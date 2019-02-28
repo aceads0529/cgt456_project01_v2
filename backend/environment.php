5159edb3e56ff0a301ff86fcaa22ecd9f4c1b941
turne248 <turne248@purdue.edu>
2019-02-27 19:24:11 -0500

    new css changes

--- backend/environment.php
+++ backend/environment.php
@@ -1,11 +1,11 @@
 <?php
 
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASS = '';
-const DB_NAME = 'cgt456_project01';
+//const DB_HOST = 'localhost';
+//const DB_USER = 'root';
+//const DB_PASS = '';
+//const DB_NAME = 'cgt456_project01';
 
-// const DB_HOST = 'www.aaroneads.com';
-// const DB_USER = 'admin';
-// const DB_PASS = 'Ascii32';
-// const DB_NAME = 'cgt456_project01';
+ const DB_HOST = 'www.aaroneads.com';
+ const DB_USER = 'admin';
+ const DB_PASS = 'Ascii32';
+ const DB_NAME = 'cgt456_project01';
